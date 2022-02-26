36fb8c882a783f07a6c7819b1badc8d978e3194e
xiaolongma <xiaolongma2020@gmail.com>
2022-02-26 16:19:20 +0800

    第一周作业：加一

---
+++ homework/src/main/java/datastructure/DoubleListNode.java
@@ -0,0 +1,20 @@
+package datastructure;
+
+/**
+ * 双链表定义
+ */
+public class DoubleListNode {
+    public int val;
+    public int index;
+    public DoubleListNode pre;
+    public DoubleListNode next;
+
+    public DoubleListNode(int val) {
+        this.val = val;
+    }
+
+    public DoubleListNode(int val, int index) {
+        this.val = val;
+        this.index = index;
+    }
+}
