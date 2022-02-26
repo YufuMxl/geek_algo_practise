aa9f45eaf79c52e6cffe963d3e628756662caebb
xiaolongma <xiaolongma2020@gmail.com>
2022-02-26 16:21:58 +0800

    第一周作业：合并两个有序链表

---
+++ homework/src/main/java/datastructure/ListNode.java
@@ -0,0 +1,18 @@
+package datastructure;
+
+/**
+ * 单链表定义
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    private ListNode() {}
+
+    public ListNode(int val) {this.val = val;}
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
