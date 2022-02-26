61c507a5c609a419993e4905968b6dad7d55a672
xiaolongma <xiaolongma2020@gmail.com>
2022-02-26 16:26:40 +0800

    第一周作业：设计循环双端队列 与 最大矩形 的模板

---
+++ homework/src/main/java/first_week/DesignCircularDeque.java
@@ -0,0 +1,44 @@
+package first_week;
+
+/**
+ * 设计循环双端队列
+ *
+ * @see <a href="https://leetcode-cn.com/problems/design-circular-deque/">LeetCode 641</a>
+ */
+public class DesignCircularDeque {
+    public DesignCircularDeque(int k) {
+
+    }
+
+    public boolean insertFront(int value) {
+        return false;
+    }
+
+    public boolean insertLast(int value) {
+        return false;
+    }
+
+    public boolean deleteFront() {
+        return false;
+    }
+
+    public boolean deleteLast() {
+        return false;
+    }
+
+    public int getFront() {
+        return -1;
+    }
+
+    public int getRear() {
+        return -1;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean isFull() {
+        return false;
+    }
+}
---
+++ homework/src/main/java/first_week/MaximalRectangle.java
@@ -0,0 +1,14 @@
+package first_week;
+
+/**
+ * 最大矩形
+ *
+ * @see <a href="https://leetcode-cn.com/problems/maximal-rectangle/">LeetCode 85</a>
+ */
+public class MaximalRectangle {
+
+    // 单调栈
+    public int maximalRectangle(char[][] matrix) {
+        return 0;
+    }
+}
